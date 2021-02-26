8b453661166c35fe0dfa6db97064a6b81c0fa46f
Vladimir Neizhmakov <vovka_x@mail.ru>
2021-02-26 16:26:58 +0300

    HW3 Исправлено в соответствии с замечаниями

--- src/main/java/com/geekbrains/spring/mvc/controllers/ProductController.java
+++ src/main/java/com/geekbrains/spring/mvc/controllers/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-// root: http://localhost:8189/app/students
+// root: http://localhost:8189/app/p
 
 @Controller
 @RequestMapping("/products")
@@ -23,7 +23,7 @@ public class ProductController {
 
     @GetMapping
     public String showAllProducts(Model model) {
-        List<Product> products = productService.getProductRepository().getProducts();
+        List<Product> products = productService.getProducts();
         model.addAttribute("products", products);
         model.addAttribute("avgCost", productService.productAverageCost());
         model.addAttribute("count", productService.productCount());
@@ -31,25 +31,25 @@ public class ProductController {
 
     @PostMapping("/add")
     public String addNewProduct(@ModelAttribute Product product) {
-        productService.getProductRepository().addProduct(product);
+        productService.addProduct(product);
         return "redirect:/products";
     }
 
     @GetMapping("/remove/{id}")
     public String deleteProductById(@PathVariable int id) {
-        productService.getProductRepository().deleteProduct(productService.getProductRepository().getListID(id));
+        productService.deleteProduct(productService.getListID(id));
         return "redirect:/products";
     }
 
     @GetMapping("/update/{id}")
     public String showEditForm(@PathVariable int id, Model model) {
-        model.addAttribute("product", productService.getProductRepository().getProducts().get(productService.getProductRepository().getListID(id)));
+        model.addAttribute("product", productService.getProducts().get(productService.getListID(id)));
         return "edit_product_form";
     }
 
     @PostMapping("/update")
     public String updateProduct(@ModelAttribute(name = "id") int id, @ModelAttribute(name = "title") String title, @ModelAttribute(name = "cost") int cost) {
-        productService.getProductRepository().updateProduct(productService.getProductRepository().getListID(id), title, cost);
+        productService.updateProduct(productService.getListID(id), title, cost);
         return "redirect:/products/";
     }
 
--- src/main/java/com/geekbrains/spring/mvc/services/ProductService.java
+++ src/main/java/com/geekbrains/spring/mvc/services/ProductService.java
@@ -2,22 +2,43 @@ package com.geekbrains.spring.mvc.services;
 
 import com.geekbrains.spring.mvc.model.Product;
 import com.geekbrains.spring.mvc.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ProductService {
 
-    public ProductRepository getProductRepository() {
-        return productRepository;
-    }
+    //public ProductRepository getProductRepository() {
+   //     return productRepository;
+  //  }
 
     private ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository){this.productRepository = productRepository;}
+    @Autowired
+    public void SetProductRepository(ProductRepository productRepository){this.productRepository = productRepository;}
+
+    public List<Product> getProducts() {
+        return productRepository.getProducts();
+    }
+
+    public void addProduct(Product product) {productRepository.addProduct(product);}
+
+    public void updateProduct(int id, String newtitle, int newcost) {
+        productRepository.updateProduct(id,newtitle,newcost);
+    }
+
+    public void deleteProduct(int id) {
+        productRepository.deleteProduct(id);
+    }
+
+    public int getListID(int productID){
+        return productRepository.getListID(productID);
+    }
 
     public int productCount(){
         return productRepository.getProducts().size();
-
     }
 
     public double productAverageCost(){
